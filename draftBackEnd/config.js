c7b754bf6068128db50f5f5266c6eee7c0bee993
scott <scott.a.benson189@gmail.com>
2021-01-06 16:04:56 -0600

    Removed API files not needed.

--- draftBackEnd/config.js
+++
@@ -1,9 +0,0 @@
-let DB_URI = `postgresql://`
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/draft`
-} else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/draft`
-}
-
-module.exports = DB_URI 
--- draftBackEnd/errorHandlers.js
+++
@@ -1,7 +0,0 @@
-const ExpressError = require('./expressError')
-
-const invalidDraftDbError = () => {
-    return new ExpressError("Invalid draft", 404)
-}
-
-exports.invalidDraftDbError = invalidDraftDbError
